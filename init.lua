7aa02c33fb5e89b060427ceeba2589b6eee808b9
115100 <git@three-sigma.org>
2025-03-28 17:23:28 +0000

    init: fix for 0.11 changes

--- init.lua
+++ init.lua
@@ -18,7 +18,13 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 
 -- Reenable swapfile prompt when the swapfile is owned by a running Nvim process
-vim.api.nvim_clear_autocmds({ group = 'nvim_swapfile' })
+local swapfile_group
+if vim.version.ge(vim.version(), '0.11.0') then
+	swapfile_group = 'nvim.swapfile'
+else
+	swapfile_group = 'nvim_swapfile'
+end
+vim.api.nvim_clear_autocmds({ group = swapfile_group })
 
 vim.api.nvim_create_autocmd({ 'FileType' }, {
 	pattern = { 'hcl', 'terraform', 'yaml', 'yml' },
@@ -28,6 +34,10 @@ vim.api.nvim_create_autocmd({ 'FileType' }, {
 		vim.bo.shiftwidth = 2
 	end
 })
+
+if vim.version.ge(vim.version(), '0.11.0') then
+	vim.diagnostic.config({ virtual_text = true })
+end
 
 require('plug')
 require('remaps')
